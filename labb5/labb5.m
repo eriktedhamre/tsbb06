5d0288e6fc46da39a6732b65a014afeaa0debccf
Erik Tedhamre <erite601@student.liu.se>
2018-12-04 10:10:36 +0100

    Changed name of dir and did some more work on labb5

--- labb5/labb5.m
+++ labb5/labb5.m
@@ -48,3 +48,38 @@ figure(6);plot([0:99],flip(e),'o');
 PM
 
 figure(8);plot3(PC(:,1)'*A0, PC(:,2)'*A0, PC(:,3)'*A0, 'o');axis('equal');
+
+%%
+
+im = double(imread('middlebury.png')); % Choose you image here!
+size(im)
+figure(10);colormap('gray');imagesc(im);
+
+N = 8;
+A = im2col(im,[N N],'distinct');
+size(A)
+
+[PC S] = svd(A);                 %Compute SVD of A
+PM = diag(S.^2);                  %magnitudes are given by the
+                                  %singular values
+figure(11);
+subplot(2,1,1);plot(PM,'o');
+subplot(2,1,2);plot(log(PM),'o');
+%%
+figure(12);colormap('gray');
+for M=1:6,
+    c=PC(:,1:M)'*A;    %Compute coordinates from blocks
+    Arec=PC(:,1:M)*c;  %Reconstruct blocks from coordinates
+    imrec=col2im(Arec,[N N],size(im),'distinct');  %Reshape into image
+    subplot(2,3,M);imagesc(imrec);axis('off');     %Display image
+    title(sprintf('%d principal components',M));   %Set title
+end
+
+%%
+
+figure(14);colormap('gray');
+for cnt=1:6,
+    subplot(2,3,cnt);h=mesh(reshape(PC(:,cnt),N,N));
+    set(h,'edgecolor','black');axis([1 N 1 N -0.5 0.5]);
+    title(sprintf('principal component %d',cnt));
+end
